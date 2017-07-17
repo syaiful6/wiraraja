600f7664b3b1533ac163275eed427a2670442fea
Syaiful Bahri <syaiful@pengunjungblog.com>
2017-07-17 10:01:48 +0000

    use package.yaml

---
+++ src/Web/Wiraraja/Routing.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Web.Wiraraja.Routing
+  ( Path
+  , Route(..)
+  , Match(..)
+  , end
+  , lit
+  , bool
+  , text
+  , method
+  , runMatch
+  , routeMiddleware
+  ) where
+
+import           Control.Applicative (Alternative, (<|>), empty)
+
+import           Data.Text (Text)
+
+import qualified Network.HTTP.Types   as H
+import           Network.Wai (Request, Application, Middleware, pathInfo, requestMethod)
+
+type Path = Text
+
+data Route = Route !H.Method [Path]
+
+newtype Match a = Match (Route -> Maybe (Route, a))
+
+-- | Match against an end path piece
+end :: Match ()
+end = Match $ \(Route m rp) -> case rp of
+    "" : []      -> Just (Route m [], ())
+    []           -> Just (Route m [], ())
+    _            -> Nothing
+
+lit :: Text -> Match ()
+lit !t = parseSegment parse
+  where
+    parse s
+      | s == t    = Just ()
+      | otherwise = Nothing
+
+bool :: Match Bool
+bool = parseSegment parse
+  where
+    parse s
+      | s == "true"  = Just True
+      | s == "false" = Just False
+      | otherwise    = Nothing
+
+text :: Match Text
+text = parseSegment Just
+
+method :: H.Method -> Match H.Method
+method !n = Match $ \(Route m rp) ->
+    if n == m then Just (Route m rp, m) else Nothing
+
+parseSegment :: (Text -> Maybe a) -> Match a
+parseSegment k = Match $ \(Route m rp) -> case rp of
+    p : ps    -> (\x -> (Route m ps, x)) <$> k p
+    _         -> Nothing
+
+runMatch :: Match a -> Route -> Maybe a
+runMatch (Match k) r = maybe Nothing (Just . snd) (k r)
+
+buildRoute :: Request -> Route
+buildRoute !req = Route (requestMethod req) (pathInfo req)
+
+routeMiddleware :: Match Application -> Middleware
+routeMiddleware match app req = case runMatch match (buildRoute req) of
+    Just routeApp -> routeApp req
+    Nothing       -> app req
+
+instance Functor Match where
+    fmap f (Match k) = Match $ \x -> maybe Nothing (Just . fmap f) (k x)
+
+instance Applicative Match where
+    pure x = Match (\r -> Just (r, x))
+    Match ff <*> Match fa = Match $ \x -> case ff x of
+        Nothing     -> Nothing
+        Just (y, f) -> case fa y of
+            Nothing     -> Nothing
+            Just (r, z) -> Just (r, f z)
+
+instance Alternative Match where
+    empty = Match $ \_ -> Nothing
+    Match fa <|> Match fb = Match $ \r -> case fa r of
+        Nothing -> fb r
+        Just x  -> Just x
---
+++ tests/Network/Wiraraja/RoutingSpec.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Web.Wiraraja.RoutingSpec (spec) where
+
+import           Control.Applicative ((<|>))
+
+import qualified Data.Text as T
+import           Data.Maybe (fromMaybe)
+
+import qualified Network.HTTP.Types as H
+import           Test.Hspec
+import           Web.Wiraraja.Routing
+
+data Page = Home
+          | About
+          | Users H.Method T.Text
+          | User H.Method T.Text
+          | NotFound
+          deriving (Eq, Ord, Show)
+
+matchRoute :: Route -> Page
+matchRoute = fromMaybe NotFound . runMatch parser
+  where
+    parser = Home <$ method H.methodGet <* end
+         <|> About <$ (method H.methodGet <* lit "about") <* end
+         <|> Users <$> postOrGet <*> (lit "users" *> text) <* end
+         <|> User <$> postOrGet <*> (lit "user" *> text) <* end
+
+postOrGet :: Match H.Method
+postOrGet = method H.methodGet <|> method H.methodPost
+
+spec :: Spec
+spec = do
+    describe "Wiraraja Routing" $ do
+        it "Can match against home page" $ do
+            let res = matchRoute $ Route H.methodGet []
+            res `shouldBe` Home
+
+        it "Match against simple route" $ do
+            let res = matchRoute $ Route H.methodGet ["about"]
+            res `shouldBe` About
+
+        it "Unequal Method should fail the parser" $ do
+            let notfound = matchRoute $ Route H.methodPost ["about"]
+            notfound `shouldBe` NotFound
+
+        it "Should capture the parameter correctly" $ do
+            let users = matchRoute $ Route H.methodPost ["users", "foo"]
+            users `shouldBe` Users H.methodPost "foo"
